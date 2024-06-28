f6e62539dc4a42b363d741d4c60ae9c91d4a92e8
LC <qq2271517853@outlook.com>
2024-06-28 23:45:11 +0800

    探索时缓下的插值.

--- Assets/Scripts/Test/DirectionalRushTest.cs
+++ Assets/Scripts/Test/DirectionalRushTest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,7 +15,15 @@ public class DirectionalRushTest : MonoBehaviour
 
     private Color clearColor;
 
-    private Vector3 mousePos;
+    private Vector3 mousePosLast;
+
+    private Vector3 mousePosCur;
+
+    private Vector3 mousePosPredict;
+
+    private Vector3 mousePosInterpolate;
+
+    private float t_update;
 
     private bool isPressing;
 
@@ -23,10 +32,41 @@ public class DirectionalRushTest : MonoBehaviour
         dirRenderer = GetComponent<LineRenderer>();
         clearColor = new Color(0, 0, 0, 0);
         isPressing = false;
+        t_update = 0;
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (isPressing)
+        {
+            mousePosCur = curMouse.position.ReadValue();
+            mousePosCur = Camera.main.ScreenToWorldPoint(mousePosCur);
+            mousePosCur.z = 0;
+            mousePosPredict = mousePosCur + (mousePosCur - mousePosLast);
+            mousePosInterpolate = math.lerp(mousePosCur, mousePosPredict, t_update / Time.fixedDeltaTime);
+
+            Debug.Log(t_update / Time.fixedDeltaTime);
+
+            dirRenderer.SetPosition(0, player.transform.position);
+            dirRenderer.SetPosition(1, mousePosCur);
+            dirRenderer.startColor = Color.blue;
+            dirRenderer.endColor = Color.green;
+
+            mousePosLast = mousePosCur;
+        }
+        else
+        {
+            dirRenderer.startColor = clearColor;
+            dirRenderer.endColor = clearColor;
+        }
+
+        t_update = 0;
     }
 
     void Update()
     {
+        t_update += Time.deltaTime;
         curMouse = Mouse.current;
         if (curMouse != null)
         {
@@ -38,22 +78,6 @@ public class DirectionalRushTest : MonoBehaviour
             {
                 isPressing = false;
             }
-        }
-
-        if (isPressing)
-        {
-            mousePos = curMouse.position.ReadValue();
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-            mousePos.z = 0;
-            dirRenderer.SetPosition(0, player.transform.position);
-            dirRenderer.SetPosition(1, mousePos);
-            dirRenderer.startColor = Color.blue;
-            dirRenderer.endColor = Color.green;
-        }
-        else
-        {
-            dirRenderer.startColor = clearColor;
-            dirRenderer.endColor = clearColor;
         }
     }
 }
